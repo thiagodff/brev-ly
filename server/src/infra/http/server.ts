d2f5cc070acd0869a3f741ea78a185f3d6a505ef
Thiago Dornelles <thiago.fdornelles@gmail.com>
2025-09-21 02:29:33 -0300

    feat(server): setup project

---
+++ server/src/infra/http/server.ts
@@ -0,0 +1,17 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.register(fastifyCors, {
+  origin: '*',
+})
+
+server
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP server running on http://0.0.0.0:3333')
+  })
