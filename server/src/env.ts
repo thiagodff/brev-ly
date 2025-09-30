020e8ecbfa7dff8c277b4926f2a6bd4e2714e606
Thiago Dornelles <thiago.fdornelles@gmail.com>
2025-09-30 00:43:09 -0300

    feat(server): setup cloudflare r2

--- server/src/env.ts
+++ server/src/env.ts
@@ -4,6 +4,11 @@ const envSchema = z.object({
   PORT: z.coerce.number(),
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   DATABASE_URL: z.url().startsWith('postgresql://'),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+  CLOUDFLARE_BUCKET: z.string(),
+  CLOUDFLARE_PUBLIC_URL: z.url(),
 })
 
 export const env = envSchema.parse(process.env)
