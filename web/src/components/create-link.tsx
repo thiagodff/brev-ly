03e9d18570a14feaaf96a2d3cebb7d9609c934db
Thiago Dornelles <thiago.fdornelles@gmail.com>
2025-10-08 16:35:52 -0300

    feat(web): react hook form to control create link form

--- web/src/components/create-link.tsx
+++ web/src/components/create-link.tsx
@@ -1,18 +1,61 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
+
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 import { Input } from "./ui/input";
 
+type Inputs = {
+  url: string;
+  slug: string;
+};
+
 export function CreateLink() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
-    <Card className="gap-5 md:gap-6 flex-5/12">
-      <h2 className="text-lg leading-6 font-bold">Novo Link</h2>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Card className="gap-5 md:gap-6 flex-5/12">
+        <h2 className="text-lg leading-6 font-bold">Novo Link</h2>
 
-      <div className="flex flex-col gap-4">
-        <Input id="url" title="Link Original" placeholder="www.exemplo.com" />
-        <Input id="slug" title="Link Encurtado" placeholder="brev.ly/" />
-      </div>
+        <div className="flex flex-col gap-4">
+          <Input
+            id="url"
+            title="Link Original"
+            error={errors.url?.message}
+            placeholder="www.exemplo.com"
+            {...register("url", {
+              required: { value: true, message: "Campo obrigatório" },
+              pattern: {
+                value:
+                  /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/,
+                message: "URL inválida",
+              },
+            })}
+          />
+          <Input
+            id="slug"
+            title="Link Encurtado"
+            error={errors.slug?.message}
+            placeholder="brev.ly/"
+            {...register("slug", {
+              maxLength: { value: 30, message: "Máximo 30 caracteres" },
+              required: { value: true, message: "Campo obrigatório" },
+              pattern: {
+                value: /^[a-zA-Z0-9-_]+$/,
+                message: "Apenas letras, números, - e _ são permitidos",
+              },
+            })}
+          />
+        </div>
 
-      <Button>Salvar Link</Button>
-    </Card>
+        <Button type="submit">Salvar Link</Button>
+      </Card>
+    </form>
   );
 }
